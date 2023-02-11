06b3bdcc2d62c4fe66019f9a0d94d3a4e579a86c
crazebotz <120011436+crazebotz@users.noreply.github.com>
2023-02-11 16:18:20 +0530

    Update and rename bot.py to main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import pyrogram
-
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-import os
-
 from config import Config
 from pyrogram import Client 
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 if __name__ == "__main__" :
@@ -17,11 +7,12 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = Client(
-        "ShowJson",
+        "Stylish_text_bot",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         plugins=plugins,
         workers=100
     )
+    print("BOT HAS RUNNING SUCCESFULLY")
     app.run()
