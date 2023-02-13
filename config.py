277d9eec52d30e02d6acedb5db8aea6430444608
crazebotz <120011436+crazebotz@users.noreply.github.com>
2023-02-13 08:56:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,1 @@
-import os
-from os import environ as env
-
-class Config(object):
-
-      BOT_TOKEN = env.get("BOT_TOKEN",'')
-      API_ID = int(env.get("API_ID",'1234'))
-      API_HASH = env.get("API_HASH",'')
-      OWNER_ID = 5294965763
-
+print('Starting')
